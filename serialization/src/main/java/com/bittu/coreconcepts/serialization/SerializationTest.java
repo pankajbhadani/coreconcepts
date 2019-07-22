fb6218ca2b63d4697f16365858af68c9812c5ee4
Pankaj Kumar Bhadani <pankaj.kumar.bhadani@ericsson.com>
2019-07-22 15:38:57 +0530

    Serialization Module and code added
    Code changes for serialization module

---
+++ serialization/src/main/java/com/bittu/coreconcepts/serialization/SerializationTest.java
@@ -0,0 +1,45 @@
+package com.bittu.coreconcepts.serialization;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.bittu.coreconcepts.serialization.entity.Address;
+import com.bittu.coreconcepts.serialization.entity.Employee;
+
+public class SerializationTest {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+
+		Employee employee = new Employee();
+		Address address = new Address();
+		address.setHouseNumber(146);
+		address.setAddress("Chhatarpur");
+		address.setLandmark("CDR Chowk");
+		address.setCity("New Delhi");
+		
+		employee.setId(1);
+		employee.setFirstName("Pankaj");
+		employee.setMiddleName("Kumar");
+		employee.setLastName("Bhadani");
+		employee.setAddress(address);
+		
+		String fileName = "data.ser";
+		String filePath = "src/main/resources/";
+		File file = new File(filePath + fileName);
+		
+		try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))) {
+			objectOutputStream.writeObject(employee);
+		}
+		
+		Employee readEmployee;
+		try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))){
+			readEmployee = (Employee)objectInputStream.readObject();
+		}
+		System.out.println(readEmployee);
+	}
+}
---
+++ serialization/src/main/java/com/bittu/coreconcepts/serialization/entity/Address.java
@@ -0,0 +1,40 @@
+package com.bittu.coreconcepts.serialization.entity;
+
+import java.io.Serializable;
+
+public class Address implements Serializable {
+	private int houseNumber;
+	private String address;
+	private String landmark;
+	private String city;
+	
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getLandmark() {
+		return landmark;
+	}
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Address [houseNumber=" + houseNumber + ", address=" + address + ", landmark=" + landmark + ", city="
+				+ city + "]";
+	}
+}
